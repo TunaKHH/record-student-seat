9d2ce3e9379588ac3974c00a216a8dc9253e9015
STUTuna <df95111111@gmail.com>
2023-06-08 16:45:15 +0800

    新增vuedraggable、vuetify、pinia和SeatLayout組件，刪除HelloWorld組件。

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <v-app>
+      <v-content>
+        <SeatLayout />
+      </v-content>
+    </v-app>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Parent from "./components/ParentCards.vue";
+import SeatLayout from "./components/SeatLayout.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // Parent,
+    SeatLayout,
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SeatLayout.vue
@@ -0,0 +1,51 @@
+<template>
+  <draggable style="margin-right: 10px" v-model="students" v-bind="dragOptions" group="group1" @start="drag = true"
+    @end="drag = false" item-key="id">
+    <template #item="{ element }">
+      <v-card variant="outlined" width="400" :title="element.name">
+        <v-card-subtitle>Tuna{{ element.subtitle }}</v-card-subtitle>
+        <v-card-text>
+          This is content
+        </v-card-text>
+      </v-card>
+
+    </template>
+  </draggable>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+import { defineComponent, ref, computed } from 'vue'
+
+export default defineComponent({
+  components: {
+    draggable
+  },
+  setup() {
+    const students = ref([
+      { id: 1, name: 'student A', subtitle: '學生的課程進度' },
+      { id: 2, name: 'student B', subtitle: '學生的課程進度' },
+      { id: 3, name: 'student C', subtitle: '學生的課程進度' },
+      { id: 4, name: 'student D', subtitle: '學生的課程進度' },
+      { id: 5, name: 'student E', subtitle: '學生的課程進度' },
+      { id: 6, name: 'student F', subtitle: '學生的課程進度' },
+      { id: 7, name: 'student G', subtitle: '學生的課程進度' },
+      { id: 8, name: 'student H', subtitle: '學生的課程進度' },
+    ])
+
+    const dragOptions = computed(() => {
+      return {
+        animation: 100,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
+      }
+    })
+
+    return {
+      students,
+      dragOptions
+    }
+  }
+})
+</script>
